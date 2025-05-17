d5f3540831763cd6e63efc18f480c984f6ac57e8
Arthur <arthurhenrrique1705@gmail.com>
2025-05-16 23:16:23 -0300

    Criação de cabeçalho e pratos principais

---
+++ espeto-esperto/src/Header.js
@@ -0,0 +1,25 @@
+import './Header.css';
+
+function Header() {
+  return (
+      <header className="App-header">
+        <img src="" id="logo" alt="Insira aqui a logo"/>
+
+        <nav>
+          <ul>
+            <li class="item"><a href="#">Inicio</a></li>
+            <li class="item"><a href="#">Sobre nós</a></li>
+            <li class="item"><a href="#">Contato</a></li>
+            <li class="item"><a href="#">Marcar hora</a></li>
+          </ul>
+        </nav>
+
+        <div id="login-cadastro-conta">
+          <button type="button">Login</button>
+          <button type="button">Cadastro</button>
+        </div>
+      </header>
+  );
+}
+
+export default Header;
---
+++ espeto-esperto/src/Principais.js
@@ -0,0 +1,25 @@
+import "./Principais.css"
+
+function Principais() {
+    return(
+        <ul>
+            <li class="prato">
+                <img src="Prato principal.png" class="foto" alt="Foto de um dos pratos principais"/>
+                <h2>Nome do prato</h2>
+                <p>Descrição do prato</p>
+            </li>
+            <li class="prato">
+                <img src="Bife welingthon.png" class="foto" alt="Foto de um dos pratos principais"/>
+                <h2>Nome do prato</h2>
+                <p>Descrição do prato</p>
+            </li>
+            <li class="prato">
+                <img src="Bife comum.png" class="foto" alt="Foto de um dos pratos principais"/>
+                <h2>Nome do prato</h2>
+                <p>Descrição do prato</p>
+            </li>
+        </ul>
+    )
+}
+
+export default Principais
