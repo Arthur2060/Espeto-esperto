1e68af9734f7fa027b00764ac6b85427660aea03
Arthur <arthurhenrrique1705@gmail.com>
2025-05-19 16:07:29 -0300

    Commit 19/05/25 Adição de aba sobre nós

--- espeto-esperto/src/SobreNos.js
+++ espeto-esperto/src/SobreNos.js
@@ -1,6 +1,11 @@
+import "./SobreNos.css"
+
 function SobreNos() {
     return (
-
+        <div id="SobreNos">
+            <p>sdadwa d ad sadadadad asdwad awdwadasdas erefada awdaawd ad awdwadafa dsada dawdad adwadawdsa dawda</p>
+            <img src="Fachada.png" alt="Foto da fachada do restaurante"/>
+        </div>
     )
 }
 
--- espeto-esperto/src/index.js
+++ espeto-esperto/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './Header';
 import Principais from "./Principais"
+import SobreNos from "./SobreNos"
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <Header />
     <Principais />
+    <SobreNos />
   </React.StrictMode>
 );
 
