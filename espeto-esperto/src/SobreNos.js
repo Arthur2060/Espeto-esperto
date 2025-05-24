f73a28f2ed3c75b1e3b71b654476923f96c5ed9d
Arthur <arthurhenrrique1705@gmail.com>
2025-05-24 11:15:42 -0300

    Refinando

--- espeto-esperto/src/SobreNos.js
+++ espeto-esperto/src/SobreNos.js
@@ -3,7 +3,9 @@ import "./SobreNos.css"
 function SobreNos() {
     return (
         <div id="SobreNos">
-            <p>sdadwa d ad sadadadad asdwad awdwadasdas erefada awdaawd ad awdwadafa dsada dawdad adwadawdsa dawda</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum hendrerit blandit euismod. 
+                Fusce elementum quam ut nisl viverra, ut iaculis leo hendrerit. Quisque viverra bibendum condimentum.
+                 Fusce eleifend tellus non.</p>
             <img src="Fachada.png" alt="Foto da fachada do restaurante"/>
         </div>
     )
